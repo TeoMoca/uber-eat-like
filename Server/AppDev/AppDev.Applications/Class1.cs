82a04458f36e77319bebb2ec6e52a3bcea2793a2
Victorien Goudeau <vgoudeau@sqli.com>
2022-12-14 10:44:36 +0100

    add missed files

---
+++ Server/AppDev/AppDev.Applications/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace AppDev.Applications
+{
+    // All the code in this file is included in all platforms.
+    public class Class1
+    {
+    }
+}
---
+++ Server/AppDev/AppDev.Infrastructure/Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace AppDev.Infrastructure
+{
+    // All the code in this file is only included on Mac Catalyst.
+    public class PlatformClass1
+    {
+    }
+}
