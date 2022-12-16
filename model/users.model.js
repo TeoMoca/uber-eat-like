441129582be5e7a39bd3e3314d5f5b2c872c647f
Téo Montlouis-Calixte <teo@get-potions.com>
2022-12-16 11:36:41 +0100

    :sparkles: create user register and login with jwt token

---
+++ model/users.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const Users = mongoose.model(
+  "Users",
+  new mongoose.Schema({
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  })
+);
+
+module.exports = Users;
--- server.js
+++ server.js
@@ -1,8 +1,32 @@
-const http = require("http");
-const host = "localhost";
-const port = 8080;
+//imports
+const express = require("express");
+const usersRouter = require("./routes/api/users");
+require("dotenv").config();
 
-const server = http.createServer((req, res) => {
-  res.end("réponse du serveur");
+const app = express();
+console.log("coucou");
+app.use("/users", usersRouter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Server Start
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Launching Server on PORT ${process.env.SERVER_PORT}`);
 });
-server.listen(port, host);
+
+//connection to MongoDB
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    `mongodb://${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DB}`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connexion à MongoDB réussie !");
+  })
+  .catch(() => {
+    console.log("Connexion à MongoDB échouée !");
+  });
